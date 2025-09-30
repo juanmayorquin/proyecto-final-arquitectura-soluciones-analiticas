97a21c785b8e6b125257a92115e99f1d25127ec3
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2025-09-29 20:40:52 -0500

    add: Workflow 'Elemento' completado

---
+++ Scripts/crearGlobal.sql
@@ -0,0 +1,14 @@
+CREATE TABLE stg1_global (
+
+    codigo_area     INT,
+    area            VARCHAR(66),
+    codigo_producto INT,
+    producto        VARCHAR(58),
+    codigo_elemento INT,
+    elemento        VARCHAR(24),
+    codigo_ano      INT,
+    ano             INT,
+    unidad          VARCHAR(12),
+    valor           DECIMAL(15,1),
+    simbolo         VARCHAR(2)
+);
