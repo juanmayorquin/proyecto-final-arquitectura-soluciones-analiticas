e97d17dabd00d2e30c15a2e8b537363f6c90897a
Saritakoala <148582998+Saritakoala@users.noreply.github.com>
2025-10-01 14:36:32 -0500

    crearHechos

---
+++ Scripts/crearHechos.sql
@@ -0,0 +1,13 @@
+CREATE TABLE stg1_hechos (
+    id_area      INT,
+    area             VARCHAR(255),
+    id_producto  INT,
+    producto         VARCHAR(255),
+    id_elemento  INT,
+    elemento         VARCHAR(255),
+    id_anio      INT,
+    anio             INT,
+    unidad           VARCHAR(100),
+    valor            NUMERIC,
+    simbolo          VARCHAR(50)
+);
