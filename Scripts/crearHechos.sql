118c92e882211143e9159eaf3b070a142398b4f8
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2025-10-02 00:04:50 -0500

    add: Se añadio workflow para hechos_comercio, y se corrigieron errores en los table inputs

--- Scripts/crearHechos.sql
+++ Scripts/crearHechos.sql
@@ -1,4 +1,4 @@
-CREATE TABLE stg1_hechos (
+CREATE TABLE stg1_hechos_comercio (
     id_area      INT,
     area             VARCHAR(255),
     id_producto  INT,
@@ -10,4 +10,13 @@ CREATE TABLE stg1_hechos (
     unidad           VARCHAR(100),
     valor            NUMERIC,
     simbolo          VARCHAR(50)
+);
+
+CREATE TABLE stg2_hechos_comercio (
+    id_area      INT,
+    id_producto  INT,
+    id_elemento  INT,
+    id_anio      INT,
+    valor        NUMERIC,
+    simbolo      VARCHAR(50)
 );
